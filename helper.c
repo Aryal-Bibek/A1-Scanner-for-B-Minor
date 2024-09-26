12b446d4520f54500fb0c8c52e1eba164d5aac57
Aryal-Bibek <77513350+Aryal-Bibek@users.noreply.github.com>
2024-09-26 19:35:26 -0400

    Add files via upload

---
+++ helper.c
@@ -0,0 +1,196 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "token.h"
+#include "helper.h"
+
+/* You may use this file to add any helper functions you need. */
+
+void printToken(int token){
+    switch(token){
+     case 0:
+        printf("TOKEN_EOF\n");
+        break;
+    
+    case 1:
+        printf("TOKEN_ADD\n");
+        break;
+
+    case 2:
+        printf("TOKEN_MULTIPLY\n");
+        break;
+
+    case 3:
+        printf("TOKEN_SUBTRACT\n");
+        break;
+
+    case 4:
+        printf("TOKEN_DIVIDE\n");
+        break;
+
+    case 5:
+        printf("TOKEN_MODULO\n");
+        break;
+
+    case 6:
+        printf("TOKEN_EXPONENT\n");
+        break;
+
+    case 7:
+        printf("TOKEN_NOT\n");
+        break;
+
+    case 8:
+        printf("TOKEN_INCREMENT\n");
+        break;
+
+    case 9:
+        printf("TOKEN_DECREMENT\n");
+        break;
+
+    case 10:
+        printf("TOKEN_WHILE\n");
+        break;
+
+    case 11:
+        printf("TOKEN_FOR\n");
+        break;
+
+    case 12:
+        printf("TOKEN_IDENT\n");
+        break;
+
+    case 13:
+        printf("TOKEN_INTEGER_LITERAL\n");
+        break;
+
+    case 14:
+        printf("TOKEN_BOOLEAN\n");
+        break;
+
+    case 15:
+        printf("TOKEN_STRING\n");
+        break;
+
+    case 16:
+        printf("TOKEN_CHAR\n");
+        break;
+
+    case 17:
+        printf("TOKEN_INTEGER\n");
+        break;
+
+    case 18:
+        printf("TOKEN_VOID\n");
+        break;
+
+    case 19:
+        printf("TOKEN_TRUE\n");
+        break;
+
+    case 20:
+        printf("TOKEN_FALSE\n");
+        break;
+
+    case 21:
+        printf("TOKEN_ARRAY\n");
+        break;
+
+    case 22:
+        printf("TOKEN_CHAR_LITERAL\n");
+        break;
+
+    case 23:
+        printf("TOKEN_STRING_LITERAL\n");
+        break;
+
+    case 24:
+        printf("TOKEN_PRINT\n");
+        break;
+
+    case 25:
+        printf("TOKEN_IF\n");
+        break;
+
+    case 26:
+        printf("TOKEN_ELSE\n");
+        break;
+
+    case 27:
+        printf("TOKEN_FUNCTION\n");
+        break;
+
+    case 28:
+        printf("TOKEN_RETURN\n");
+        break;
+
+    case 29:
+        printf("TOKEN_EQ\n");
+        break;
+
+    case 30:
+        printf("TOKEN_NE\n");
+        break;
+
+    case 31:
+        printf("TOKEN_GE\n");
+        break;
+
+    case 32:
+        printf("TOKEN_LE\n");
+        break;
+
+    case 33:
+        printf("TOKEN_ASSIGNMENT\n");
+        break;
+
+    case 34:
+        printf("TOKEN_GT\n");
+        break;
+
+    case 35:
+        printf("TOKEN_LT\n");
+        break;
+
+    case 36:
+        printf("TOKEN_SEMICOLON\n");
+        break;
+
+    case 37:
+        printf("TOKEN_AND\n");
+        break;
+
+    case 38:
+        printf("TOKEN_OR\n");
+        break;
+
+    case 39:
+        printf("TOKEN_LB\n");
+        break;
+
+    case 40:
+        printf("TOKEN_RB\n");
+        break;
+
+    case 41:
+        printf("TOKEN_CLB\n");
+        break;
+
+    case 42:
+        printf("TOKEN_CRB\n");
+        break;
+    
+     case 43:
+        printf("TOKEN_SLB\n");
+        break;
+    
+     case 44:
+        printf("TOKEN_SRB\n");
+        break;
+    
+
+     case 100:
+        printf("TOKEN_ERROR\n");
+        break;
+}
+}
---
+++ scanner.flex
@@ -0,0 +1,62 @@
+%{
+#include "token.h"
+#include "helper.h"
+%}
+
+%option nounput
+%option noinput
+%option yylineno
+
+DIGIT  [0-9]
+LETTER [a-zA-Z]
+%%
+(" "|\t|\n|\/\/.*|\/*.*\*\/)  /* skip whitespace */
+
+ \+                             { return TOKEN_ADD;}
+ \*                             { return TOKEN_MULTIPLY;}
+\-                               { return TOKEN_SUBTRACT;}
+\/                               { return TOKEN_DIVIDE;}
+%                               { return TOKEN_MODULO;}
+\^                               { return TOKEN_EXPONENT;}
+!                               { return TOKEN_NOT;}
+\+\+                              { return TOKEN_INCREMENT;}
+\-\-                              { return TOKEN_DECREMENT;}
+while                           { return TOKEN_WHILE; }
+for                             { return TOKEN_FOR;}
+char                            { return TOKEN_CHAR;}
+boolean                         { return TOKEN_BOOLEAN;}
+string                          { return TOKEN_STRING;}
+integer                         { return TOKEN_INTEGER;}
+void                            { return TOKEN_VOID;}
+true                            { return TOKEN_TRUE;}
+false                           { return TOKEN_FALSE;}
+array                           { return TOKEN_ARRAY;}
+print                           { return TOKEN_PRINT;}
+if                              { return TOKEN_IF;}
+else                            { return TOKEN_ELSE;}
+function                        { return TOKEN_FUNCTION;}
+return                          { return TOKEN_RETURN;}
+(_|{LETTER})(_|{LETTER}|{DIGIT})+                                    { return TOKEN_IDENT; }
+{DIGIT}+                        { return TOKEN_INTEGER_LITERAL; }
+'{LETTER}|{DIGIT}'         { return TOKEN_CHAR_LITERAL;}
+"(LETTER|DIGIT)*"               { return TOKEN_STRING_LITERAL;}
+==                              { return TOKEN_EQ;}
+!=                              { return TOKEN_NE;}
+>=                              { return TOKEN_GE;}
+\<=                              { return TOKEN_LE;}
+=                               { return TOKEN_ASSIGNMENT;}
+>                               { return TOKEN_GT;}
+\<                               { return TOKEN_LT;}
+;                               { return TOKEN_SEMICOLON;}
+&&                              { return TOKEN_AND;}
+\|\|                            { return TOKEN_OR;}
+\(                              { return TOKEN_LB;}    
+\)                              { return TOKEN_RB;}
+\{                              { return TOKEN_CLB;}
+\}                              { return TOKEN_CRB;}
+\[                              {return TOKEN_SLB;}
+\]                              {return TOKEN_SRB;}
+.                               { return TOKEN_ERROR;}
+%%
+
+int yywrap() { return 1; }
