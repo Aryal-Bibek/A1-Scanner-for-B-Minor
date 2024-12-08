2852d5ba76f0544dab50ecc11312b04f65e9f1f5
Benjamin Michailov <benmich@rl25.eecs.yorku.ca>
2024-12-08 15:11:35 -0500

    added basic arithmetic and rudimentary function defintion

--- lab2/codegen.c
+++ lab2/codegen.c
@@ -5,6 +5,14 @@
 #include "expr.h"
 #include "stmt.h"
 #include "decl.h"
+#include "symbol.h"
+
+const char * symbol_codegen (struct symbol * s){
+    ///TODO : this function returns the exact location of given symbol (i.e. MOVQ -8(%rbp), %rbx)
+    const char *  hi;
+    
+    return hi;
+}
 
 void expr_codegen(struct expr * e){
     if (!e) return ;
@@ -13,38 +21,52 @@ void expr_codegen(struct expr * e){
         // Leaf node: allocate register and load value.
         case EXPR_NAME:
             e->reg = scratch_alloc();
-            printf("MOVQ %s, %s\n",
-            symbol_codegen(e->symbol),
-            scratch_name(e->reg));
+            printf("MOVQ %s, %s\n",symbol_codegen(e->symbol),scratch_name(e->reg));
             break;
 
         // Interior node: generate children, then add them.
         case EXPR_ADD:
             expr_codegen(e->left);
             expr_codegen(e->right);
-            printf("ADDQ %s, %s\n",
-            scratch_name(e->left->reg),
-            scratch_name(e->right->reg));
+            printf("ADDQ %s, %s\n",scratch_name(e->left->reg),scratch_name(e->right->reg));
             e->reg = e->right->reg;
             scratch_free(e->left->reg);
             break;
 
         case EXPR_ASSIGN:
             expr_codegen(e->left);
-            printf("MOVQ %s, %s\n",
-            scratch_name(e->left->reg),
-            symbol_codegen(e->right->symbol));
+            printf("MOVQ %s, %s\n", scratch_name(e->left->reg), symbol_codegen(e->right->symbol));
             e->reg = e->left->reg;
             break;
 
         // TODO : add the rest of the cases.
         case EXPR_SUB:
+            expr_codegen(e->left);
+            expr_codegen(e->right);
+            printf("SUBQ %s, %s\n",scratch_name(e->left->reg),scratch_name(e->right->reg));
+            e->reg = e->right->reg;
+            scratch_free(e->left->reg);
             break;
         case EXPR_MUL:
+            expr_codegen(e->left);
+            expr_codegen(e->right);
+            printf("MOVQ %s %%rax\n",scratch_name(e->left->reg));
+            printf("IMULQ %s\n", scratch_name(e->right->reg));
+            printf("MOVQ %%rax %s\n",scratch_name(e->right->reg));
+            scratch_free(e->left->reg);
             break;
         case EXPR_DIV:
+            expr_codegen(e->left);
+            expr_codegen(e->right);
+            printf("MOVQ %s %%rax\n",scratch_name(e->left->reg));
+            printf("CQO\n");
+            printf("IDIVQ %s\n", scratch_name(e->right->reg));
+            printf("MOVQ %%rax %s\n",scratch_name(e->right->reg));
+            scratch_free(e->left->reg);
             break;
         case EXPR_INTEGER_LITERAL:
+            e->reg = scratch_alloc();
+            printf("MOVQ %s, $%d\n",scratch_name(e->reg),e->literal_value);
             break;
         case EXPR_BOOLEAN_LITERAL:
             break;
@@ -67,6 +89,13 @@ void expr_codegen(struct expr * e){
         case EXPR_EXP:
             break;
         case EXPR_MOD:
+            expr_codegen(e->left);
+            expr_codegen(e->right);
+            printf("MOVQ %s %%rax\n",scratch_name(e->left->reg));
+            printf("CQO\n");
+            printf("IDIVQ %s\n", scratch_name(e->right->reg));
+            printf("MOVQ %%rdx %s\n",scratch_name(e->right->reg));
+            scratch_free(e->left->reg);
             break;
         case EXPR_LT:
             break;
@@ -102,7 +131,7 @@ void stmt_codegen( struct stmt *s )
             break;
         case STMT_RETURN:
             expr_codegen(s->expr);
-            printf("MOV %s, %%rax\n",scratch_name(s->expr->reg));
+            printf("MOVQ %s, %%rax\n",scratch_name(s->expr->reg));
             printf("JMP .%s_epilogue\n",s->decl->name);
             scratch_free(s->expr->reg);
             break;
@@ -134,4 +163,66 @@ void stmt_codegen( struct stmt *s )
 
 void decl_codegen(struct decl * d){
     // function epilogues must have unique labels so that return statemtnts can jump there easily
+    if (!d) return;
+    
+    printf("%d\n",d->type->kind);
+    if (d->type->kind == TYPE_FUNCTION){
+        printf("in type func of decl codegen\n");
+        /*
+        pushq %rbp # save the base pointer
+movq %rsp, %rbp # set new base pointer
+pushq %rdi # save first argument on the stack
+pushq %rsi # save second argument on the stack
+pushq %rdx # save third argument on the stack
+subq $16, %rsp # allocate two more local variables
+pushq %rbx # save callee-saved registers
+pushq %r12
+pushq %r13
+pushq %r14
+pushq %r15
+
+### body of function goes here ###
+popq %r15 # restore callee-saved registers
+popq %r14
+popq %r13
+popq %r12
+popq %rbx
+movq %rbp, %rsp # reset stack pointer
+popq %rbp # recover previous base pointer
+ret # return to the caller
+        
+        */
+
+        printf("PUSHQ %%rbp\n");
+        printf("MOVQ %%rsp, %%rbp\n");
+        /*while
+        printf("MOVQ %s",)*/
+
+        printf("PUSHQ %%rdi\n");
+        printf("PUSHQ %%rsi\n");
+        printf("PUSHQ %%rdx\n");
+        printf("SUBQ $16, %%rsp\n");
+        printf("PUSHQ %%rbx\n");
+        printf("PUSHQ %%r10\n");
+        printf("PUSHQ %%r11\n");
+        printf("PUSHQ %%r12\n");
+        printf("PUSHQ %%r13\n");
+        printf("PUSHQ %%r14\n");
+        printf("PUSHQ %%r15\n");
+
+        stmt_codegen(d->code);
+        printf(".%s_epilogue:\n", d->name);
+        printf("POPQ %%r15\n");
+        printf("POPQ %%r14\n");
+        printf("POPQ %%r13\n");
+        printf("POPQ %%r12\n");
+        printf("POPQ %%r11\n");
+        printf("POPQ %%r10\n");
+        printf("POPQ %%rbx\n");
+        printf("MOVQ %%rbp, %%rsp\n");
+        printf("POPQ %%rbp\n");
+        printf("RET\n");
+    }
+    decl_codegen(d->next);
 }
+
---
+++ lab2/codegen.h
@@ -0,0 +1,7 @@
+void expr_codegen(struct expr * e);
+
+void stmt_codegen( struct stmt *s );
+
+void decl_codegen(struct decl * d);
+
+const char * symbol_codegen (struct symbol * s);
