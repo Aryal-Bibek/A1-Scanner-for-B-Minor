448614abbe5a6447f45a3585d541a93e14c09cfb
Benjamin Michailov <benmich@rl25.eecs.yorku.ca>
2024-12-08 17:27:15 -0500

    handles global variables and function calls now

--- lab2/codegen.c
+++ lab2/codegen.c
@@ -7,11 +7,16 @@
 #include "decl.h"
 #include "symbol.h"
 
+int data_label_emitted = 0;
+
+int decl_codegen_called_by_stmt = 0;
+
 const char * symbol_codegen (struct symbol * s){
-    ///TODO : this function returns the exact location of given symbol (i.e. MOVQ -8(%rbp), %rbx)
-    const char *  hi;
+    ///this function returns the exact location of given symbol (i.e. MOVQ -8(%rbp), %rbx)
     
-    return hi;
+    char * symbol_location = (char*)malloc(99 * sizeof(char));
+    sprintf(symbol_location,"%d(%%rbp)", s->which*8);
+    return symbol_location;
 }
 
 void expr_codegen(struct expr * e){
@@ -66,7 +71,7 @@ void expr_codegen(struct expr * e){
             break;
         case EXPR_INTEGER_LITERAL:
             e->reg = scratch_alloc();
-            printf("MOVQ %s, $%d\n",scratch_name(e->reg),e->literal_value);
+            printf("MOVQ $%d ,%s \n",e->literal_value,scratch_name(e->reg));
             break;
         case EXPR_BOOLEAN_LITERAL:
             break;
@@ -75,8 +80,25 @@ void expr_codegen(struct expr * e){
         case EXPR_STRING_LITERAL:
             break;
         case EXPR_CALL:
+            struct expr * argument = e->right;
+            int argument_counter = 0;
+            while (argument && argument_counter < 3){
+                if (argument_counter ==0){
+                    printf("MOVQ %s %%rdi\n",scratch_name(argument->left->reg));
+                } else if (argument_counter==1){
+                    printf("MOVQ %s %%rsi\n",scratch_name(argument->left->reg));
+                } else if (argument_counter==2){
+                    printf("MOVQ %s %%rdx\n",scratch_name(argument->left->reg));
+                }
+                argument_counter++;
+                argument = argument->right;
+            }
+            printf("JMP .%s",e->left->name);
             break;
         case EXPR_ARG:
+            expr_codegen(e->left);
+            expr_codegen(e->right);
+            //printf("")
             break;
         case EXPR_SUBSCRIPT:
             break;
@@ -127,7 +149,9 @@ void stmt_codegen( struct stmt *s )
             scratch_free(s->expr->reg);
             break;
         case STMT_DECL:
+            decl_codegen_called_by_stmt=1;
             decl_codegen(s->decl);
+            decl_codegen_called_by_stmt=0;
             break;
         case STMT_RETURN:
             expr_codegen(s->expr);
@@ -148,7 +172,6 @@ void stmt_codegen( struct stmt *s )
             stmt_codegen(s->else_body);
             printf("%s:\n",label_name(done_label));
             break;
-
 	    case STMT_IF_ELSE:
             break;
         case STMT_FOR:
@@ -165,63 +188,100 @@ void decl_codegen(struct decl * d){
     // function epilogues must have unique labels so that return statemtnts can jump there easily
     if (!d) return;
     
-    printf("%d\n",d->type->kind);
+    //printf("%d\n",d->type->kind);
     if (d->type->kind == TYPE_FUNCTION){
-        printf("in type func of decl codegen\n");
-        /*
-        pushq %rbp # save the base pointer
-movq %rsp, %rbp # set new base pointer
-pushq %rdi # save first argument on the stack
-pushq %rsi # save second argument on the stack
-pushq %rdx # save third argument on the stack
-subq $16, %rsp # allocate two more local variables
-pushq %rbx # save callee-saved registers
-pushq %r12
-pushq %r13
-pushq %r14
-pushq %r15
+       // printf("in type func of decl codegen\n");
 
-### body of function goes here ###
-popq %r15 # restore callee-saved registers
-popq %r14
-popq %r13
-popq %r12
-popq %rbx
-movq %rbp, %rsp # reset stack pointer
-popq %rbp # recover previous base pointer
-ret # return to the caller
+        if (d->code /*if this is the function defintion*/){
+            //if you have more than three parameters, you are out of luck with our compiler
+            printf(".%s:\n", d->name);
+
+            printf("PUSHQ %%rbp\n");
+            printf("MOVQ %%rsp, %%rbp\n");
+
+            printf("PUSHQ %%rdi\n");
+            printf("PUSHQ %%rsi\n");
+            printf("PUSHQ %%rdx\n");
+
+            printf("SUBQ $16, %%rsp\n");
+            printf("PUSHQ %%rbx\n");
+            printf("PUSHQ %%r10\n");
+            printf("PUSHQ %%r11\n");
+            printf("PUSHQ %%r12\n");
+            printf("PUSHQ %%r13\n");
+            printf("PUSHQ %%r14\n");
+            printf("PUSHQ %%r15\n");
+
+            stmt_codegen(d->code);
+            printf(".%s_epilogue:\n", d->name);
+            printf("POPQ %%r15\n");
+            printf("POPQ %%r14\n");
+            printf("POPQ %%r13\n");
+            printf("POPQ %%r12\n");
+            printf("POPQ %%r11\n");
+            printf("POPQ %%r10\n");
+            printf("POPQ %%rbx\n");
+            printf("MOVQ %%rbp, %%rsp\n");
+            printf("POPQ %%rbp\n");
+            printf("RET\n");
+        }
+    } else {
+        if (decl_codegen_called_by_stmt == 0 && data_label_emitted == 0){
+            data_label_emitted++;
+            printf(".data\n");
+        }
+        if (decl_codegen_called_by_stmt == 0){
+            printf("%s: ", d->name);
+            if (d->type->kind==TYPE_STRING){
+                printf(".string ");
+                if (d->value){
+                    printf("\"%s\"\n",d->value->string_literal);
+                } else{
+                    printf("\n");
+                }
+            } else if (d->type->kind != TYPE_ARRAY){
+                printf(".quad ");
+                if (d->value){
+                    printf("%d\n",d->value->literal_value);
+                } else{
+                    printf("\n");
+                }
+            } else {
+                if (d->type->subtype->kind==TYPE_STRING){
+                    printf(".string");
+                    struct expr * arg  = d->value;
+                    int goneAround =0;
+                    while (arg&&arg->left){
+                        if (goneAround !=0){
+                            printf(",");
+                        }
+                        printf(" \"%s\"",arg->left->string_literal);
+                        goneAround=1;
+                        arg=arg->right;
+                    }
+                    printf("\n");
+                } else {
+                    printf(".string");
+                    struct expr * arg  = d->value;
+                    int goneAround =0;
+                    while (arg&&arg->left){
+                        if (goneAround !=0){
+                            printf(",");
+                        }
+                        printf(" %d",arg->left->literal_value);
+                        goneAround=1;
+                        arg=arg->right;
+                    }
+                    printf("\n");
+                } 
+            }
+        } else {
+            if (d->value){
+                expr_codegen(d->value);
+                printf("MOVQ %s, %s\n",scratch_name(d->value->reg),symbol_codegen(d->symbol));
+            }
+        }
         
-        */
-
-        printf("PUSHQ %%rbp\n");
-        printf("MOVQ %%rsp, %%rbp\n");
-        /*while
-        printf("MOVQ %s",)*/
-
-        printf("PUSHQ %%rdi\n");
-        printf("PUSHQ %%rsi\n");
-        printf("PUSHQ %%rdx\n");
-        printf("SUBQ $16, %%rsp\n");
-        printf("PUSHQ %%rbx\n");
-        printf("PUSHQ %%r10\n");
-        printf("PUSHQ %%r11\n");
-        printf("PUSHQ %%r12\n");
-        printf("PUSHQ %%r13\n");
-        printf("PUSHQ %%r14\n");
-        printf("PUSHQ %%r15\n");
-
-        stmt_codegen(d->code);
-        printf(".%s_epilogue:\n", d->name);
-        printf("POPQ %%r15\n");
-        printf("POPQ %%r14\n");
-        printf("POPQ %%r13\n");
-        printf("POPQ %%r12\n");
-        printf("POPQ %%r11\n");
-        printf("POPQ %%r10\n");
-        printf("POPQ %%rbx\n");
-        printf("MOVQ %%rbp, %%rsp\n");
-        printf("POPQ %%rbp\n");
-        printf("RET\n");
     }
     decl_codegen(d->next);
 }
